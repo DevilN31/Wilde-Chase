4432b518283e39fc9fba95c06c4190d56cafdce1
Nataniel Tsimerman <deviln3107@hotmail.com>
2020-09-20 15:22:50 +0300

    Testing Part 2
    - Made AI Wagon for player
    - Made a simple path for testing movement
    - Camera Control script is just for making the video NOT for game use

---
+++ Assets/Game Factory/Scripts/CameraControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public Vector3 setPosition;
+    public Vector3 setRotaion;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void MoveToTransform()
+    {
+        this.transform.position = setPosition;
+        this.transform.rotation = Quaternion.Euler(setRotaion);
+    }
+}
