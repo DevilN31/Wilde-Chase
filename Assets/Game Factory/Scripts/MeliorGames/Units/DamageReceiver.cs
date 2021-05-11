8f9ec904de14b5ed72adae144370645c48120c1b
Andrii Baranchuk <andrii.baranchuk@meliorgames.com>
2021-05-11 17:54:13 +0300

    Added environment prefabs.

--- Assets/Game Factory/Scripts/MeliorGames/Units/DamageReceiver.cs
+++ Assets/Game Factory/Scripts/MeliorGames/Units/DamageReceiver.cs
@@ -21,6 +21,9 @@ namespace Game_Factory.Scripts.MeliorGames.Units
     {
       if (!other.collider.TryGetComponent(out Projectile projectile)) return;
       if (projectile.Team == Team) return;
+
+      projectile.Rigidbody.velocity = Vector3.zero;
+      projectile.Thrown = false;
       
       Debug.Log("Damage Received");
       DamageReceived?.Invoke();
