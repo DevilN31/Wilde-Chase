85e20e9fde9bf7fd4be75e7b1064f48d4c4a7a2c
Andrii Baranchuk <andrii.baranchuk@meliorgames.com>
2021-05-14 19:18:41 +0300

    Added NextLevelPopUp.

---
+++ Assets/Game Factory/Scripts/MeliorGames/UI/PopUp/NextLevelPopUp.cs
@@ -0,0 +1,30 @@
+﻿using Game_Factory.Scripts.MeliorGames.Infrastructure;
+using Game_Factory.Scripts.MeliorGames.TimeService;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Game_Factory.Scripts.MeliorGames.UI.PopUp
+{
+  public class NextLevelPopUp : GameplayPopUp
+  {
+    public Button NextLevelButton;
+    
+    private LoadingCurtain loadingCurtain;
+    
+    public void Init(LoadingCurtain _loadingCurtain)
+    {
+      loadingCurtain = _loadingCurtain;
+    }
+
+    private void Start()
+    {
+      NextLevelButton.onClick.AddListener(() =>
+      {
+        TimeControl.Instance.RunGame();
+        loadingCurtain.Show();
+        loadingCurtain.Hide();
+        Close();
+      });
+    }
+  }
+}
